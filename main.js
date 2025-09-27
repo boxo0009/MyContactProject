5e5bd8131fbe69e0ec90e3c281d7a708e377ab5d
boxo0009 <b37928799@gmail.com>
2025-09-27 18:21:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,125 @@
+const privateContacts = document.querySelector(".private-contacts");
+const loader = document.querySelector(".loader");
+const myContact = document.querySelector(".my-contact");
+const darkBtn = document.getElementById("darkmode");
+const lightBtn = document.getElementById("lightmode");
+let searchInput = document.querySelector(".search-box input")
+
+async function getData() {
+  loader.style.display = "inline-block";
+  try {
+    let res = await fetch(
+      "https://68ce76ca6dc3f350777f0e06.mockapi.io/contacts/users"
+    );
+    let data = await res.json();
+    console.log(data);
+    renderData(data);
+    searchInput.addEventListener("input", function () {
+      let searchData = data.filter((item) => item.name.toLowerCase().trim().includes(searchInput.value.trim().toLowerCase()))
+      if (searchData.legnth == 0) {
+        myContact.innerHTML = 'CONTACT NOT FOUND'
+      } else {
+        myContact.innerHTML = null
+        renderData(searchData)
+      }
+    })
+
+
+
+
+
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loader.style.display = "none";
+  }
+}
+
+darkBtn.addEventListener("click", () => {
+  document.body.classList.add("dark");
+});
+
+lightBtn.addEventListener("click", () => {
+  document.body.classList.remove("dark");
+});
+
+getData();
+
+function renderData(data) {
+  data.map((contact) => {
+    myContact.innerHTML += `
+
+      <div id="myContact">
+      <div>
+        <h4>${contact.name}</h4>
+        <p>${contact.number}</p>
+      </div>
+      <div>
+        <i onclick="updateData(${contact.id})"  class="fa-solid fa-pen"></i>
+        <i onclick="deleteData(${contact.id})" class="fa-solid fa-trash"></i>
+      </div>
+      </div>
+
+    `;
+  });
+}
+
+
+async function deleteData(id) {
+  try {
+    let response = await fetch(`https://68ce76ca6dc3f350777f0e06.mockapi.io/contacts/users/${id}`, {
+      method: 'DELETE'
+    })
+    if (response.ok === true) {
+      myContact.innerHTML = null
+      alert("contact deleted")
+      getData()
+    } else {
+      alert('contact not deleted')
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+
+
+async function updateData(id) {
+  try {
+
+    let newName = prompt("new name kiriting")
+    let newNumber = +prompt("new number kiriting")
+
+    let res = await fetch(`https://68ce76ca6dc3f350777f0e06.mockapi.io/contacts/users/${id}`, {
+      method: 'PUT',
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        name: newName,
+        number: newNumber
+      })
+    })
+
+    if (res.ok == true) {
+      alert("contact edited")
+      myContact.innerHTML = null
+      getData()
+    } else {
+      alert("contact not edited")
+
+    }
+
+
+
+  } catch (error) {
+    console.log(error);
+
+  }
+}
+
+
+
+
